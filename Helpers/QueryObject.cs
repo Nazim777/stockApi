92995e04d0ee6ae72c3e3e7b4f2032093f8f0b62
Nazim777 <iamnazimhosssain@gmail.com>
2024-03-04 09:38:33 +0600

    sorting

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -10,5 +10,9 @@ namespace stockApi.Helpers
         // prop
       public string? Symbol { get; set; } = null;
         public string? CompanyName { get; set; } = null;
+         public string? SortBy { get; set; } = null;
+        public bool IsDecsending { get; set; } = false;
+        // public int PageNumber { get; set; } = 1;
+        // public int PageSize { get; set; } = 20;
     }
 }
